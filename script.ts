c868ac5971909123211dc75297ec7cb37b080bc1
fbern <fbernardi32@gmail.com>
2022-05-09 16:51:47 -0300

    estacionamento

---
+++ script.ts
@@ -0,0 +1,38 @@
+interface Veiculo
+
+(function () { 
+    const $ = (query : string): HTMLInputElement |  null  => document.querySelector(query);
+
+
+    function patio( ){ 
+        function ler ( ) { }
+
+        function adicionar ( ) { }
+
+        function remover ( ) { }
+
+        function  salvar ( ) { }
+
+        function render ( ) { }
+
+        return { ler, adicionar, remover , salvar , render}:
+    }
+
+    $("cadastrar")?.addEventListener ("click", ( ) => {
+        const nome = $("$nome")?.value;
+        const placa = $("$placa")?.value;
+       
+
+        if(!nome || !placa)   {
+            alert("Os campos e placas são obrigátorios");
+            return;
+        
+        
+            
+
+        }
+      
+
+    })
+
+  }) (  );
